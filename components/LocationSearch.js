8a3c24563456239e4a1361ec2d75eff9817b3c1d
fuadzilla911 <fuadhajiabdi78@gmail.com>
2025-05-12 15:11:40 -0400

    Update LocationSearch.js

--- components/LocationSearch.js
+++ components/LocationSearch.js
@@ -1,1 +1,16 @@
-
+export default function LocationSearch() {
+  const [query, setQuery] = useState('');
+  
+  const searchHalalPlaces = async () => {
+    const res = await fetch(`/api/halal?location=${query}`);
+    return await res.json();
+  };
+  
+  return (
+    <input 
+      type="text" 
+      onChange={(e) => setQuery(e.target.value)}
+      placeholder="Find halal restaurants..."
+    />
+  )
+}
