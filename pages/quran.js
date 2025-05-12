de816c86045b94e4e95ea3c22665b9e26760c0c7
fuadzilla911 <fuadhajiabdi78@gmail.com>
2025-05-12 18:15:53 -0400

    Update quran.js

--- pages/quran.js
+++ pages/quran.js
@@ -1,1 +1,10 @@
+// Quran-web/pages/quran.js
+export async function getAllSurahs() {
+  const response = await fetch('https://api.alquran.cloud/v1/surah');
+  return await response.json();
+}
 
+export async function getAyahs(surahNumber) {
+  const response = await fetch(`https://api.alquran.cloud/v1/surah/${surahNumber}/en.asad`);
+  return await response.json();
+}
