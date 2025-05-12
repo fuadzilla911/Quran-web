cbd6863819231dbfd26fe66c78244bf76542eb63
fuadzilla911 <fuadhajiabdi78@gmail.com>
2025-05-12 15:13:03 -0400

    Update halal.js

--- pages/api/halal.js
+++ pages/api/halal.js
@@ -1,1 +1,9 @@
+import axios from 'axios';
 
+export default async function handler(req, res) {
+  const { location } = req.query;
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=halal+${location}&key=${process.env.GOOGLE_API_KEY}`
+  );
+  res.status(200).json(response.data.results);
+}
